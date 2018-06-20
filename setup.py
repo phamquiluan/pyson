a1a5754d68af10a91c84f86db7a60ebe8e266c19
Vo Anh <Bi@Vos-MacBook-Pro.local>
2018-06-20 14:10:23 +0700

    0.1

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+    name="unet",
+    author="anson",
+    author_email="anson@cinnamon.is",
+    version='0.1',
+    description="A python package covers some unet for segmentation task"
+)
