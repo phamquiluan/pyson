5133ff8382409dead2f477710834547cb5e84a7e
Vo Anh <Bi@Vos-MacBook-Pro.local>
2018-06-21 17:16:31 +0700

    1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='pyson',
       version='1.0',
       description='A package for unet models',
-      url='https://github.com/vnbot2/unet.git',
+      url='https://github.com/vnbot2/pyson.git',
       author='Anh Vo Tran Hai',
       author_email='anson@cinnamon.is',
       license='CINNAMON',
