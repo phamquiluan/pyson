b593434bd603f33a1ad14ab91b454e8ab63dd666
Vo Anh <Bi@Vos-MacBook-Pro.local>
2018-06-21 17:17:19 +0700

    1.0

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='pyson',
       author='Anh Vo Tran Hai',
       author_email='anson@cinnamon.is',
       license='CINNAMON',
-      packages=['unet', 'im_utils'],
+      packages=['unet', 'imgutils'],
       zip_safe=False)
