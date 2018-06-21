26a314cc99eb609ee0e23bebbcf06ba46b66277a
Vo Anh <Bi@Vos-MacBook-Pro.local>
2018-06-21 17:08:59 +0700

    1.0

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='unet1',
+setup(name='pyson',
       version='0.1',
       description='A package for unet models',
       url='https://github.com/vnbot2/unet.git',
