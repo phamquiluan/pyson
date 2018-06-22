3435ffa8d41fae17549634916cec85e6e9a8cd4e
Vo Anh <Bi@Vos-MacBook-Pro.local>
2018-06-22 16:45:14 +0700

    1.0

--- utils/json.py
+++ utils/json.py
@@ -6,6 +6,6 @@ def load_json_from_path(path):
         Input: path to json file
         Return: A dictionary of the json file
     '''
-    with open('output/retrain/frozen/options.json', 'r') as f:
+    with open(path, 'r') as f:
         data = json.load(f)
     return data
